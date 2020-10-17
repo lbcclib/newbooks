ebbf4deb8a3ea7eb8b9d08bb20c4fbb2be2218a6
Jane Sandberg <sandbej@linnbenton.edu>
2020-10-16 23:49:10 -0700

    More work on book display

--- _plugins/books_from_evergreen_generator.rb
+++ _plugins/books_from_evergreen_generator.rb
@@ -2,6 +2,7 @@
 
 ATOM_NAMESPACE = 'http://www.w3.org/2005/Atom'
 DC_NAMESPACE = 'http://purl.org/dc/elements/1.1/'
+HOLDINGS_NAMESPACE = 'http://open-ils.org/spec/holdings/v1'
 
 require 'nokogiri'
 require 'open-uri'
@@ -14,6 +15,8 @@ class BooksFromEvergreenGenerator < Jekyll::Generator
   def generate(site)
     new_records = records_from_evergreen site.config
     site.data['books'] = new_records
+    departments = site.config['departments'].map { |department| Department.new department }
+    #departments.each { |department| Department.email_about site.data['books'] }
   end
 
   private
@@ -25,20 +28,38 @@ class BooksFromEvergreenGenerator < Jekyll::Generator
           "/#{config['num_items_to_fetch']}"\
           '?status=0&status=1&status=6&status=7'
     xml = Nokogiri::XML(URI.open(url))
-    xml.xpath('//atom:entry', 'atom' => ATOM_NAMESPACE).map { |entry| Book.new(entry) }
+    xml.xpath('//atom:entry', 'atom' => ATOM_NAMESPACE)
+       .map { |entry| Book.new(entry) }
+       .select(&:cover_image) # we only want books with cover images
   end
 end
 
 # A new book
 class Book
+  attr_reader :cover_image
+
   def initialize(entry)
     @author = entry.at_xpath('./atom:author', 'atom' => ATOM_NAMESPACE)
     @author = @author.text if @author
-    @call_number = 'cat'
-    @uri = 'cat'
+
+    @call_number = entry.at_xpath(
+      './holdings:holdings/holdings:volumes/holdings:volume/@label',
+      'holdings' => HOLDINGS_NAMESPACE
+    ).text
+
+    @uri = 'https://libfind.linnbenton.edu:4430/catalog/'\
+           "#{entry.at_xpath('./atom:id', 'atom' => ATOM_NAMESPACE).text[/\d+/]}"
     @title = entry.at_xpath('./atom:title', 'atom' => ATOM_NAMESPACE).text
-    @date_cataloged = 'cat'
-    @shelving_location = 'cat'
+
+    @date_cataloged = entry.at_xpath('./atom:updated', 'atom' => ATOM_NAMESPACE)
+    @date_cataloged = @date_cataloged if @date_cataloged
+
+    @shelving_location = entry.at_xpath(
+      './holdings:holdings/holdings:volumes/holdings:volume/holdings:label/holdings:copies/holdings:copy/holdings:location',
+      'holdings' => HOLDINGS_NAMESPACE
+    )
+    @shelving_location = @shelving_location.text if @shelving_location
+
     @cover_image = cover_url_for entry.xpath('./dc:identifier[text()[contains(.,"URN:ISBN")]]', 'dc' => DC_NAMESPACE)
   end
 
@@ -55,15 +76,15 @@ class Book
   end
 
   def has_image?
-    !@cover_image.nil?
+    @cover_image
   end
 
   private
 
   def cover_url_for(isbns)
     isbn_with_image = isbns.detect { |isbn| image_exists(isbn) }
-    ol_url(isbn_with_image, 'M') if isbn_with_image
-    isbn_with_image ? ol_url(isbn_with_image) : 'http://placekitten.com/g/200/300'
+    ol_url(isbn_with_image) if isbn_with_image
+    isbn_with_image ? ol_url(isbn_with_image) : nil
   end
 
   def ol_url(isbn)
@@ -71,13 +92,10 @@ class Book
   end
 
   def image_exists(isbn)
-    return false if normalize_isbn(isbn).empty?
-    puts normalize_isbn(isbn)
+    return false if normalize_isbn(isbn).nil?
 
     begin
-      puts "LOOKING"
       response = Net::HTTP.get_response(URI.parse(ol_url(isbn)))
-      puts response
     end
     return false if response.body.include? 'not found'
     return true if %w[200 301 302].include? response.code
@@ -87,6 +105,13 @@ class Book
 
   def normalize_isbn(isbn)
     # Evergreen delivers ISBNs in the format URN:ISBN:0123456789
-    isbn.text.sub 'URN:ISBN', ''
+    isbn.text[/[X0-9]{10,13}/]
+  end
+end
+
+# An LBCC department
+class Department
+  def initialize(config)
+    puts config
   end
 end
--- index.md
+++ index.md
@@ -6,9 +6,12 @@ title: New books
 <div class="grid" data-masonry='{ "itemSelector": ".grid-item", "columnWidth": 200 }'>
   {% for book in site.data.books %}
     <div class="grid-item">
-      <img src="{{ book.cover_image }}">
-      Title: {{ book.title }}
-      Call number: {{ book.call_number }}
+      <a href="{{ book.uri }}">
+        <img src="{{ book.cover_image }}" alt="">
+        <h2>{{ book.title }}</h2>
+      </a>
+      Call number: {{ book.call_number }}<br />
+      Shelving location: {{ book.call_number }}
     </div>
   {% endfor %}
 </div>
